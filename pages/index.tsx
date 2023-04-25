2a598d1c28ad49873b22b240212bf1c631d91aa2
AmeenIzhac <ameen.izhac@gmail.com>
2023-04-25 21:11:44 +0100

    dw

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/index.module.scss";
 
-interface HorizontalGapTypes {
-  gap: number;
-}
-
 interface DropdownBoxTypes {
   options: string[];
 }
@@ -60,13 +56,10 @@ function ColumnTitles() {
 function FiltersDiv() {
   return (
     <div className={styles.filtersDiv}>
-      <HorizontalGap gap={90} />
       <InputBox />
-      <HorizontalGap gap={170} />
       <DropdownBox
         options={["", "Subcontractor 1", "Subcontractor 2", "Subcontractor 3"]}
       />
-      <HorizontalGap gap={170} />
       <DropdownBox
         options={["", "Product Type 1", "Product Type 2", "Product Type 3"]}
       />
@@ -76,42 +69,46 @@ function FiltersDiv() {
 
 function InputBox() {
   return (
-    <div className={styles.inputBox}>
-      <span className="text-gray-400 mr-3">
-        <svg
-          className="h-5 w-5"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M21 21l-4.35-4.35"></path>
-          <circle cx="8.5" cy="8.5" r="7.5"></circle>
-        </svg>
-      </span>
-      <input
-        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-        type="text"
-        placeholder="Search..."
-      />
+    <div className={styles.centerHorizontally}>
+      <div className={styles.inputBox}>
+        <span className="text-gray-400 mr-3">
+          <svg
+            className="h-5 w-5"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M21 21l-4.35-4.35"></path>
+            <circle cx="8.5" cy="8.5" r="7.5"></circle>
+          </svg>
+        </span>
+        <input
+          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
     </div>
   );
 }
 
 function DropdownBox({ options }: DropdownBoxTypes) {
   return (
-    <div className={styles.dropdownBox}>
-      <select className="block appearance-none w-full bg-transparent border border-gray-300 text-gray-700 h-8 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-        {options.map((option, index) => {
-          return <option key={index}>{option}</option>;
-        })}
-      </select>
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-          <path d="M10 12l-6-6 1.41-1.41L10 9.17l4.59-4.58L16 6l-6 6z" />
-        </svg>
+    <div className={styles.centerHorizontally}>
+      <div className={styles.dropdownBox}>
+        <select className="block appearance-none w-full bg-transparent border border-gray-300 text-gray-700 h-8 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+          {options.map((option, index) => {
+            return <option key={index}>{option}</option>;
+          })}
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
+            <path d="M10 12l-6-6 1.41-1.41L10 9.17l4.59-4.58L16 6l-6 6z" />
+          </svg>
+        </div>
       </div>
     </div>
   );
@@ -120,7 +117,6 @@ function DropdownBox({ options }: DropdownBoxTypes) {
 function Records() {
   return (
     <div className={styles.recordsContainer}>
-      <HorizontalGap gap={130} />
       <RecordsColumn
         entries={[
           "10/04/2023",
@@ -131,7 +127,6 @@ function Records() {
           "...",
         ]}
       />
-      <HorizontalGap gap={240} />
       <RecordsColumn
         entries={[
           "Cilantro",
@@ -142,7 +137,6 @@ function Records() {
           "...",
         ]}
       />
-      <HorizontalGap gap={210} />
       <RecordsColumn
         entries={[
           "Electical Cables",
@@ -153,7 +147,6 @@ function Records() {
           "...",
         ]}
       />
-      <HorizontalGap gap={290} />
       <RecordsColumn entries={["xxx", "xxx", "xxx", "xxx", "xxx", "..."]} />
     </div>
   );
@@ -167,8 +160,4 @@ function RecordsColumn({ entries }: RecordsColumnTypes) {
       })}
     </div>
   );
-}
-
-function HorizontalGap({ gap }: HorizontalGapTypes) {
-  return <div style={{ width: gap }} />;
 }
